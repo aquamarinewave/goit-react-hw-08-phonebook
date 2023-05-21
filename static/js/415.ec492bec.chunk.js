93b397f06287d0b6c0c59014cbc4fd02fcbea2bd
aquamarinewave <50908486+aquamarinewave@users.noreply.github.com>
2023-05-21 15:15:41 +0000

    Deploying to gh-pages from @ aquamarinewave/goit-react-hw-08-phonebook@671594153ee585868e839eddcbb38b228e475ee3 🚀

---
+++ static/js/415.ec492bec.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(184);function c(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"Phonebook welcome page"})})}}}]);
+//# sourceMappingURL=415.ec492bec.chunk.js.map
